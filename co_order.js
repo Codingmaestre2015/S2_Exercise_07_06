5fe496b019ee5e7d81499bfccb2ea876e81ffdca
Patrick Clawson <patrick.clawson@west-mec.org>
2018-03-27 18:11:02 -0700

    Updates to form programs
    Updated forms to calculate cost, tax, subtotal and total cost.

--- co_order.js
+++ co_order.js
@@ -32,6 +32,15 @@ window.addEventListener("load", function() {
 
     // Calculate the cost of the order
     calcOrder();
+
+    // Event handlers for the web form
+    orderForm.elements.model.onchange = calcOrder;
+    orderForm.elements.qty.onchange = calcOrder;
+
+    var planOptions = document.querySelectorAll('input[name="protection"]');
+    for (var i = 0; i < planOptions.length; i++) {
+        planOptions[i].onclick = calcOrder;
+    }
 });
 
 //Calculates the cost of the customer order
@@ -46,5 +55,32 @@ function calcOrder() {
 
     // Initial cost = cost x quantity
     var initialCost = mCost * quantity;
-    orderForm.elements.initialCost.value = initialCost;
+    orderForm.elements.initialCost.value = formatUSCurrency(initialCost);
+
+    // Retrieve the cost of the user's protection plan
+    var pCost = document.querySelector('input[name="protection"]:checked').value * quantity;
+    orderForm.elements.protectionCost.value = formatNumber(pCost, 2);
+
+    // Calculate the order subtotal
+    orderForm.elements.subtotal.value = formatNumber(initialCost + pCost, 2);
+
+    // Calculate the sales tax
+    var salesTax = 0.05 * (initialCost + pCost);
+    orderForm.elements.salesTax.value = formatNumber(salesTax, 2);
+
+    // Calculate the cost of the total order
+    var totalCost = salesTax + initialCost + pCost;
+    orderForm.elements.totalCost.value = formatUSCurrency(totalCost);
+
+    // Store the order details
+    orderForm.elements.modelName.value = orderForm.elements.model.options[mIndex].text;
+    orderForm.elements.protectionName.value = document.querySelector('input[name="protection"]:checked').nextSibling.nodeValue;
+}
+
+function formatNumber(val, decimals) {
+    return val.toLocaleString(undefined, { minimumFractionDigits: decimals, maximumFractionDigits: decimals });
+}
+
+function formatUSCurrency(val) {
+    return val.toLocaleString('en-US', { style: "currency", currency: "USD" });
 }
